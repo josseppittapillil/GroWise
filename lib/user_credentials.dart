ca88d9cce9c370698325aecac8b1a744cddd704b
Josse P Pittappillil <65817274+josseppittapillil@users.noreply.github.com>
2023-11-22 21:12:15 +0530

    Add files via upload
    commited

---
+++ lib/user_credentials.dart
@@ -0,0 +1,13 @@
+// user_credentials.dart
+
+class UserCredentials {
+  String username;
+  String email;
+  String password;
+
+  UserCredentials({
+    required this.username,
+    required this.email,
+    required this.password,
+  });
+}
