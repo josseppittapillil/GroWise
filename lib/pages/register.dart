0ea0e37f6ef696d9d8b634a9b3a8999bb5e69277
Josse P Pittappillil <65817274+josseppittapillil@users.noreply.github.com>
2023-11-22 21:08:48 +0530

    Update register.dart

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,180 +1,186 @@
 import 'package:flutter/material.dart';
-import 'package:growwise/pages/front.dart';
-import 'package:growwise/pages/home.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:growwise/pages/login.dart';
 
-class RegisterPage extends StatelessWidget {
-  const RegisterPage({super.key});
+class UserCredentials {
+  String username;
+  String email;
+  String password;
+
+  UserCredentials({
+    required this.username,
+    required this.email,
+    required this.password,
+  });
+}
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    const double fem = 1.0; // Replace with your actual scaling factor
-    const double ffem = 1.0; // Replace with your actual scaling factor
-
     return Scaffold(
-      resizeToAvoidBottomInset: true,
       body: SingleChildScrollView(
-        child: Container(
-          padding:
-              const EdgeInsets.fromLTRB(20 * fem, 45 * fem, 20 * fem, 0 * fem),
-          width: double.infinity,
-          decoration: const BoxDecoration(
-            color: Color(0xffffffff),
-          ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              IconButton(
-                icon: const Icon(Icons.arrow_back),
-                onPressed: () {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => const FrontPage()),
-                  );
-                },
-              ),
-              const SizedBox(
-                  height: 15 *
-                      fem), // Add spacing between back button and next widget
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20 * fem),
-                width: double.infinity,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Place the pin on Your accurate location on the map',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontFamily: 'Urbanist',
-                        fontSize: 15 * ffem,
-                        fontWeight: FontWeight.w600,
-                        color: Color(0xffffffff),
-                      ),
-                    ),
-                    const SizedBox(
-                        height:
-                            15 * fem), // Add spacing between text and TextField
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(
-                          19.94 * fem, 19 * fem, 19.94 * fem, 18 * fem),
-                      width: 330 * fem,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: const Color(0xffdadada)),
-                        color: const Color(0xfff7f8f9),
-                        borderRadius: BorderRadius.circular(8 * fem),
-                      ),
-                      child: const TextField(
-                        decoration: InputDecoration(
-                          hintText: 'Username',
-                          border: InputBorder.none,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                        height: 15 *
-                            fem), // Add spacing between TextField and next widget
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(
-                          19.94 * fem, 19 * fem, 19.94 * fem, 18 * fem),
-                      width: 330 * fem,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: const Color(0xffdadada)),
-                        color: const Color(0xfff7f8f9),
-                        borderRadius: BorderRadius.circular(8 * fem),
-                      ),
-                      child: const TextField(
-                        decoration: InputDecoration(
-                          hintText: 'Email',
-                          border: InputBorder.none,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                        height: 15 *
-                            fem), // Add spacing between TextField and next widget
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(
-                          19.94 * fem, 19 * fem, 19.94 * fem, 18 * fem),
-                      width: 330 * fem,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: const Color(0xffdadada)),
-                        color: const Color(0xfff7f8f9),
-                        borderRadius: BorderRadius.circular(8 * fem),
-                      ),
-                      child: const TextField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          hintText: 'Password',
-                          border: InputBorder.none,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                        height: 15 *
-                            fem), // Add spacing between TextField and next widget
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(
-                          20 * fem, 18 * fem, 20 * fem, 19 * fem),
-                      width: 330 * fem,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: const Color(0xffdadada)),
-                        color: const Color(0xfff7f8f9),
-                        borderRadius: BorderRadius.circular(8 * fem),
-                      ),
-                      child: const TextField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          hintText: 'Confirm password',
-                          border: InputBorder.none,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                        height: 80 *
-                            fem), // Add spacing between TextField and Button
-                  ],
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(
-                    35 * fem, 18 * fem, 0 * fem, 19 * fem),
-                width: 330 * fem,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const HomeScreen()),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.zero,
-                    backgroundColor: const Color(0xff02841e),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8 * fem),
-                    ),
-                  ),
-                  child: const SizedBox(
-                    width: double.infinity,
-                    height: 56 * fem,
-                    child: Center(
-                      child: Text(
-                        'Agree and Register',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontFamily: 'Urbanist',
-                          fontSize: 15 * ffem,
-                          fontWeight: FontWeight.w600,
-                          color: Color(0xffffffff),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
+              const SizedBox(height: 30),
+              _buildBackButton(context),
+              const SizedBox(height: 100.0),
+              _buildTextField('Username', _usernameController),
+              const SizedBox(height: 16.0),
+              _buildTextField('Email', _emailController),
+              const SizedBox(height: 16.0),
+              _buildTextField('Password', _passwordController,
+                  obscureText: true),
+              const SizedBox(height: 16.0),
+              _buildTextField('Confirm Password', _confirmPasswordController,
+                  obscureText: true),
+              const SizedBox(height: 32.0),
+              _buildRegisterButton(context),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildBackButton(BuildContext context) {
+    return Align(
+      alignment: Alignment.topLeft,
+      child: IconButton(
+        icon: const Icon(Icons.arrow_back),
+        onPressed: () {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => const LoginPage()),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildTextField(String labelText, TextEditingController controller,
+      {bool obscureText = false}) {
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        border: Border.all(color: const Color(0xffdadada)),
+        color: const Color(0xfff7f8f9),
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: TextField(
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          labelText: labelText,
+          border: InputBorder.none,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildRegisterButton(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () async {
+        // Check if the passwords match
+        if (_passwordController.text != _confirmPasswordController.text) {
+          // Show error dialog
+          _showErrorDialog('Error', 'Passwords don\'t match');
+          return;
+        }
+
+        // Store user credentials
+        final userCredentials = UserCredentials(
+          username: _usernameController.text,
+          email: _emailController.text,
+          password: _passwordController.text,
+        );
+
+        await saveUserCredentials(userCredentials);
+
+        // Show success message
+        _showSuccessDialog('Success', 'Registered Successfully');
+
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const LoginPage()),
+        );
+      },
+      style: ElevatedButton.styleFrom(
+        foregroundColor: Colors.white,
+        backgroundColor: const Color(0xff02841e), // Text color
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+      ),
+      child: const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text('Register Now'),
+      ),
+    );
+  }
+
+  Future<void> saveUserCredentials(UserCredentials credentials) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    // Retrieve existing user credentials list
+    final List<String> userCredentialsList =
+        prefs.getStringList('userCredentialsList') ?? [];
+
+    // Add the new user credentials to the list
+    userCredentialsList.add(
+      '${credentials.username},${credentials.email},${credentials.password}',
+    );
+
+    // Save the updated list to SharedPreferences
+    prefs.setStringList('userCredentialsList', userCredentialsList);
+  }
+
+  void _showErrorDialog(String title, String content) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(title),
+        content: Text(content),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('OK'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showSuccessDialog(String title, String content) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(title),
+        content: Text(content),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('OK'),
+          ),
+        ],
       ),
     );
   }
