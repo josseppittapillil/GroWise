e522ef44922a5e7aac8da5dfba6f5614ef3546bd
Josse P Pittappillil <65817274+josseppittapillil@users.noreply.github.com>
2023-11-22 21:08:22 +0530

    Update login.dart

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,38 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:growwise/pages/front.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:growwise/pages/register.dart';
+import 'package:growwise/pages/home.dart'; // Import your home page
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+class UserCredentials {
+  String username;
+  String email;
+  String password;
+
+  UserCredentials({
+    required this.username,
+    required this.email,
+    required this.password,
+  });
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    const double fem = 1.0; // Replace with your actual scaling factor
-    const double ffem = 1.0; // Replace with your actual scaling factor
-
     return Scaffold(
-      body: SizedBox(
-        width: double.infinity,
-        height: 812 * fem,
-        child: Container(
-          padding:
-              const EdgeInsets.fromLTRB(40 * fem, 55 * fem, 0 * fem, 85 * fem),
-          width: double.infinity,
-          height: double.infinity,
-          decoration: const BoxDecoration(
-            color: Color(0xffffffff),
-          ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              _buildBackButton(context),
-              const Spacer(),
-              _buildInputContainer('Enter your email', Icons.email),
-              _buildInputContainerWithIcon('Enter your password', Icons.lock),
-              const Spacer(),
+              const SizedBox(
+                height: 300,
+              ),
+              _buildLogoTextField('Email', Icons.email, _emailController),
+              const SizedBox(height: 16.0),
+              _buildLogoTextField('Password', Icons.lock, _passwordController,
+                  obscureText: true),
+              const SizedBox(height: 100.0),
               _buildLoginButton(context),
-              _buildRegisterNowText(context),
+              const SizedBox(height: 30.0),
+              _buildRegisterNowButton(context),
             ],
           ),
         ),
@@ -40,75 +53,29 @@ class LoginPage extends StatelessWidget {
     );
   }
 
-  Widget _buildBackButton(BuildContext context) {
+  Widget _buildLogoTextField(
+      String labelText, IconData icon, TextEditingController controller,
+      {bool obscureText = false}) {
     return Container(
-      margin: const EdgeInsets.only(top: 20),
-      child: IconButton(
-        onPressed: () {
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(builder: (context) => const FrontPage()),
-          );
-        },
-        icon: const Icon(Icons.arrow_back),
-      ),
-    );
-  }
-
-  Widget _buildInputContainer(String hintText, IconData icon) {
-    const double fem = 1.0; // Replace with your actual scaling factor
-    const double ffem = 1.0; // Replace with your actual scaling factor
-
-    return Container(
-      margin: const EdgeInsets.fromLTRB(0, 0, 0, 15),
-      padding: const EdgeInsets.fromLTRB(19.94, 19, 19.94, 18),
-      width: 330 * fem,
+      padding: const EdgeInsets.all(8.0),
       decoration: BoxDecoration(
         border: Border.all(color: const Color(0xffdadada)),
         color: const Color(0xfff7f8f9),
-        borderRadius: BorderRadius.circular(8 * fem),
-      ),
-      child: TextField(
-        decoration: InputDecoration(
-          hintText: hintText,
-          border: InputBorder.none,
-          icon: Icon(icon),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildInputContainerWithIcon(String hintText, IconData icon) {
-    const double fem = 1.0; // Replace with your actual scaling factor
-    const double ffem = 1.0; // Replace with your actual scaling factor
-
-    return Container(
-      margin: const EdgeInsets.fromLTRB(0, 0, 0, 15),
-      padding: const EdgeInsets.fromLTRB(20, 17, 9.97, 17),
-      width: 330 * fem,
-      decoration: BoxDecoration(
-        border: Border.all(color: const Color(0xffdadada)),
-        color: const Color(0xfff7f8f9),
-        borderRadius: BorderRadius.circular(8 * fem),
+        borderRadius: BorderRadius.circular(8.0),
       ),
       child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          Icon(icon, color: const Color(0xff02841e)), // Green color for icon
+          const SizedBox(width: 8.0),
           Expanded(
             child: TextField(
-              obscureText: true,
+              controller: controller,
+              obscureText: obscureText,
               decoration: InputDecoration(
-                hintText: hintText,
+                labelText: labelText,
                 border: InputBorder.none,
-                icon: Icon(icon),
               ),
             ),
-          ),
-          IconButton(
-            onPressed: () {
-              // Toggle password visibility
-            },
-            icon: const Icon(Icons.visibility),
           ),
         ],
       ),
@@ -116,93 +83,92 @@ class LoginPage extends StatelessWidget {
   }
 
   Widget _buildLoginButton(BuildContext context) {
-    const double fem = 1.0; // Replace with your actual scaling factor
-    const double ffem = 1.0; // Replace with your actual scaling factor
+    return ElevatedButton(
+      onPressed: () async {
+        // Retrieve user credentials list from SharedPreferences
+        final prefs = await SharedPreferences.getInstance();
+        final List<String> userCredentialsList =
+            prefs.getStringList('userCredentialsList') ?? [];
 
-    return Container(
-      margin: const EdgeInsets.only(top: 30),
-      child: ElevatedButton(
-        onPressed: () {},
-        style: ElevatedButton.styleFrom(
-          padding: EdgeInsets.zero,
-          backgroundColor: const Color(0xff02841e),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8),
-          ),
+        // Check if entered email and password match any stored credentials
+        final enteredCredentials =
+            '${_emailController.text},${_passwordController.text}';
+        if (userCredentialsList.contains(enteredCredentials)) {
+          // Show success message
+          _showSuccessDialog('Success', 'Login Successful');
+
+          // Redirect to the home page
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => const HomeScreen()),
+          );
+        } else {
+          // Show error message
+          _showErrorDialog('Error', 'Invalid User Credentials');
+        }
+      },
+      style: ElevatedButton.styleFrom(
+        foregroundColor: Colors.white,
+        backgroundColor: const Color(0xff02841e), // Text color
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
         ),
-        child: const SizedBox(
-          width: 330 * fem,
-          height: 56 * fem,
-          child: Center(
-            child: Text(
-              'Login',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontFamily: 'Urbanist',
-                fontSize: 15 * ffem,
-                fontWeight: FontWeight.w600,
-                color: Color(0xffffffff),
-              ),
-            ),
-          ),
-        ),
+      ),
+      child: const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text('Login'),
       ),
     );
   }
 
-  Widget _buildRegisterNowText(BuildContext context) {
-    const double fem = 1.0; // Replace with your actual scaling factor
-    const double ffem = 1.0; // Replace with your actual scaling factor
+  Widget _buildRegisterNowButton(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        // Redirect to the register page
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const RegisterPage()),
+        );
+      },
+      style: TextButton.styleFrom(
+        foregroundColor: const Color(0xff02841e), // Green color
+      ),
+      child: const Text('Register Now'),
+    );
+  }
 
-    return Container(
-      margin: const EdgeInsets.only(top: 20, left: 120),
-      child: TextButton(
-        onPressed: () {
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(builder: (context) => const RegisterPage()),
-          );
-        },
-        style: TextButton.styleFrom(
-          padding: EdgeInsets.zero,
-        ),
-        child: RichText(
-          textAlign: TextAlign.center,
-          text: const TextSpan(
-            style: TextStyle(
-              fontFamily: 'Poppins',
-              fontSize: 15 * ffem,
-              fontWeight: FontWeight.w600,
-              height: 1.4 * ffem / fem,
-              letterSpacing: 0.15 * fem,
-              color: Color(0xff02841e),
-            ),
-            children: [
-              TextSpan(
-                text: ' ',
-                style: TextStyle(
-                  fontFamily: 'Urbanist',
-                  fontSize: 15 * ffem,
-                  fontWeight: FontWeight.w500,
-                  height: 1.4 * ffem / fem,
-                  letterSpacing: 0.15 * fem,
-                  color: Color(0xff02841e),
-                ),
-              ),
-              TextSpan(
-                text: 'Register Now',
-                style: TextStyle(
-                  fontFamily: 'Urbanist',
-                  fontSize: 15 * ffem,
-                  fontWeight: FontWeight.w700,
-                  height: 1.4 * ffem / fem,
-                  letterSpacing: 0.15 * fem,
-                  color: Color(0xff02841e),
-                ),
-              ),
-            ],
+  void _showErrorDialog(String title, String content) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(title),
+        content: Text(content),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('OK'),
           ),
-        ),
+        ],
+      ),
+    );
+  }
+
+  void _showSuccessDialog(String title, String content) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(title),
+        content: Text(content),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('OK'),
+          ),
+        ],
       ),
     );
   }
