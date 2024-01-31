4b3d0d44ea5193346f22af08d0cebe88b431968a
Josse P Pittappillil <65817274+josseppittapillil@users.noreply.github.com>
2024-01-31 09:41:46 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,94 +1,61 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:growise/pages/front.dart';
 
-class YourPage extends StatelessWidget {
-  const YourPage({Key? key});
+void main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const double fem = 1.0; // Replace with your actual scaling factor
-    const double ffem = 1.0; // Replace with your actual scaling factor
+    return MaterialApp(
+      title: 'GroWise',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
 
-    return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: 812 * fem, // Adjust the height accordingly
-        child: SizedBox(
-          width: double.infinity,
-          height: double.infinity,
-          child: Stack(
-            children: [
-              GoogleMap(
-                initialCameraPosition: const CameraPosition(
-                  target: LatLng(
-                      37.7749, -122.4194), // Set the initial map location
-                  zoom: 10.0,
-                ),
-                markers: {
-                  const Marker(
-                    markerId: MarkerId('Your Location'),
-                    position:
-                        LatLng(37.7749, -122.4194), // Set the marker position
-                    infoWindow: InfoWindow(title: 'Your Location'),
-                  ),
-                },
-              ),
-              Positioned(
-                left: 23 * fem,
-                top: 718 * fem,
-                child: TextButton(
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                    padding: EdgeInsets.zero,
-                  ),
-                  child: SizedBox(
-                    width: 330 * fem,
-                    height: 56 * fem,
-                    child: Container(
-                      color: Color(0xff02841e),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8 * fem),
-                      ),
-                      child: const Center(
-                        child: Text(
-                          'Choose Your Location',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontFamily: 'Urbanist',
-                            fontSize: 15 * ffem,
-                            fontWeight: FontWeight.w600,
-                            height: 1.2 * ffem / fem,
-                            color: Color(0xffffffff),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Positioned(
-                left: 20 * fem,
-                top: 63 * fem,
-                child: Align(
-                  child: SizedBox(
-                    width: 41 * fem,
-                    height: 41 * fem,
-                    child: TextButton(
-                      onPressed: () {},
-                      style: TextButton.styleFrom(
-                        padding: EdgeInsets.zero,
-                      ),
-                      child: Image.network(
-                        '[Image url]',
-                        width: 41 * fem,
-                        height: 41 * fem,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Add a delay of 5 seconds before navigating to the 'front' function
+    Future.delayed(const Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const FrontPage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FlutterLogo(size: 100), // Replace with your app logo or image
+            SizedBox(height: 16),
+            Text(
+              'GroWise',
+              style: TextStyle(fontSize: 24),
+            ),
+          ],
         ),
       ),
     );
