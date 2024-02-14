3defe419c6d1757c40d8a36d4d515e124c769985
Nitin-Tomas31 <133589606+Nitin-Tomas31@users.noreply.github.com>
2024-02-14 12:47:45 +0530

    api key added

--- lib/pages/aibot.dart
+++ lib/pages/aibot.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class AiBotScreen extends StatefulWidget {
   const AiBotScreen({Key? key}) : super(key: key);
@@ -82,16 +84,49 @@ class _AiBotScreenState extends State<AiBotScreen> {
     );
   }
 
-  void _sendMessage() {
+  void _sendMessage() async {
     String message = _messageController.text.trim();
     if (message.isNotEmpty) {
       setState(() {
         _messages.insert(0, ChatMessage(text: message, isUser: true));
-        // Call your AI service here and handle the AI response
-        _messages.insert(
-            0, const ChatMessage(text: 'AI Response', isUser: false));
         _messageController.clear();
       });
+
+      // Call DialoGPT API to get the AI response
+      String aiResponse = await getAIResponse(message);
+
+      setState(() {
+        _messages.insert(0, ChatMessage(text: aiResponse, isUser: false));
+      });
+    }
+  }
+
+  Future<String> getAIResponse(String message) async {
+    String url = 'https://api.openai.com/v1/completions';
+    Map<String, String> headers = {
+      'Content-Type': 'application/json',
+      'Authorization':
+          'Bearer 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',
+    };
+    Map<String, dynamic> body = {
+      'model': 'gpt-3.5-turbo-instruct', // DialoGPT model
+      'prompt': message,
+      'max_tokens': 150, // Adjust based on the desired response length
+    };
+
+    http.Response response = await http.post(
+      Uri.parse(url),
+      headers: headers,
+      body: json.encode(body),
+    );
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = json.decode(response.body);
+      return data['choices'][0]['text'].toString();
+    } else {
+      print('Failed to get AI response: ${response.statusCode}');
+      print('Response body: ${response.body}');
+      throw Exception('Failed to get AI response');
     }
   }
 }
