2d437cfdf27f77580d2b894f36a9a58c09e9a61c
Nitin-Tomas31 <133589606+Nitin-Tomas31@users.noreply.github.com>
2024-02-19 15:41:35 +0530

    added prompts

---
+++ lib/pages/prompts.dart
@@ -0,0 +1,41 @@
+
+class Constants {
+  static const List<String> greetings = [
+    "hi",
+    "hello",
+    "hey",
+    "good morning",
+    "good afternoon",
+    "good evening"
+  ];
+
+  static const List<String> agricultureKeywords = [
+    "agriculture",
+    "farming",
+    "plant",
+    "plants",
+    "crops",
+    "crop",
+    "tools",
+    "tool",
+    "agronomy",
+    "horticulture",
+    "agritech",
+    "botany",
+    "soil",
+    "harvest",
+    "irrigation",
+    "gardening",
+    "fertilizer",
+    "pesticide",
+    "disease",
+    "discoloration",
+    "ph",
+    "sunlight",
+    "water",
+    "equipment",
+    "sow",
+    "reap"
+    // Add more agriculture-related keywords here
+  ];
+}
