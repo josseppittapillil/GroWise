d07a9511d5b754ee0920c2ab2aea5f31fed49a0c
nimishanigel <133152452+nimishanigel@users.noreply.github.com>
2024-02-28 20:49:05 +0530

    Add files via upload

---
+++ lib/pages/admin/adminhome.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:growise/pages/access.dart';
+
+class AdminHome extends StatelessWidget {
+  const AdminHome({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Image.asset('assets/images/back.png'),
+          onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => const AccessPage()),
+                    );
+                  },
+        ),
+        title: Text('Admin Home'),
+        // You can customize other properties of the AppBar here
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(255, 255, 255, 1),
+        ),
+      ),
+    );
+  }
+}
+
+  
