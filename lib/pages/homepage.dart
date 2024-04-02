55cc20734f1512492bafb44960c399a015a8b439
Josse P Pittappillil <65817274+josseppittapillil@users.noreply.github.com>
2024-04-02 20:56:37 +0530

    added weather widget and book appointment widget

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,9 +1,12 @@
+//import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:growise/pages/newsletter.dart';
+import 'package:growise/pages/weather.dart';
 import 'Market.dart';
 
 import 'package:fancy_bottom_navigation_plus/fancy_bottom_navigation_plus.dart';
-import 'package:growise/pages/home.dart';
+// import 'package:growise/pages/home.dart';
 import 'package:growise/pages/aibot.dart';
 
 class BottomNavExample extends StatefulWidget {
@@ -57,7 +60,7 @@ class NotificationPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text('Notification Page Content'),
+        child: Text('No Notifications'),
       ),
     );
   }
@@ -73,7 +76,7 @@ class Homepage extends StatelessWidget {
 
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.fromLTRB(20 * fem, 30 * fem, 19 * fem, 66 * fem),
+        padding: EdgeInsets.fromLTRB(20 * fem, 85 * fem, 20 * fem, 20 * fem),
         width: double.infinity,
         decoration: const BoxDecoration(
           color: Color.fromARGB(20, 254, 255, 255),
@@ -81,69 +84,69 @@ class Homepage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const SizedBox(height: 41),
-            Container(
-              margin: EdgeInsets.fromLTRB(0 * fem, 0 * fem, 0 * fem, 28 * fem),
-              width: 147 * fem,
-              height: 26 * fem,
-              child: Stack(
-                children: [
-                  Positioned(
-                    left: 0 * fem,
-                    top: 0 * fem,
-                    child: Align(
-                      child: SizedBox(
-                        width: 146 * fem,
-                        height: 26 * fem,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10 * fem),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    left: 5 * fem,
-                    top: 1 * fem,
-                    child: Align(
-                      child: SizedBox(
-                        width: 120 * fem,
-                        height: 24 * fem,
-                        child: TextButton(
-                          onPressed: () {
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const HomeScreen()),
-                            );
-                          },
-                          style: TextButton.styleFrom(
-                            padding: EdgeInsets.zero,
-                          ),
-                          child: Text(
-                            'Change Crop',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: 19 * ffem,
-                              fontWeight: FontWeight.w600,
-                              height: 1.2 * ffem / fem,
-                              color: const Color(0xff02841e),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            // const SizedBox(height: 35),
+            // Container(
+            //   margin: EdgeInsets.fromLTRB(0 * fem, 0 * fem, 0 * fem, 28 * fem),
+            //   width: 147 * fem,
+            //   height: 26 * fem,
+            //   child: Stack(
+            //     children: [
+            //       Positioned(
+            //         left: 0 * fem,
+            //         top: 0 * fem,
+            //         child: Align(
+            //           child: SizedBox(
+            //             width: 146 * fem,
+            //             height: 26 * fem,
+            //             child: Container(
+            //               decoration: BoxDecoration(
+            //                 borderRadius: BorderRadius.circular(10 * fem),
+            //               ),
+            //             ),
+            //           ),
+            //         ),
+            //       ),
+            //       Positioned(
+            //         left: 5 * fem,
+            //         top: 1 * fem,
+            //         child: Align(
+            //           child: SizedBox(
+            //             width: 120 * fem,
+            //             height: 24 * fem,
+            //             child: TextButton(
+            //               onPressed: () {
+            //                 Navigator.pushReplacement(
+            //                   context,
+            //                   MaterialPageRoute(
+            //                       builder: (context) => const HomeScreen()),
+            //                 );
+            //               },
+            //               style: TextButton.styleFrom(
+            //                 padding: EdgeInsets.zero,
+            //               ),
+            //               child: Text(
+            //                 'Change Crop',
+            //                 textAlign: TextAlign.center,
+            //                 style: TextStyle(
+            //                   fontSize: 19 * ffem,
+            //                   fontWeight: FontWeight.w600,
+            //                   height: 1.2 * ffem / fem,
+            //                   color: const Color(0xff02841e),
+            //                 ),
+            //               ),
+            //             ),
+            //           ),
+            //         ),
+            //       ),
+            //     ],
+            //   ),
+            // ),
 
             // Image and Text Section
             Container(
-              margin: EdgeInsets.fromLTRB(0 * fem, 0 * fem, 0 * fem, 17 * fem),
+              margin: EdgeInsets.fromLTRB(0 * fem, 0 * fem, 0 * fem, 0 * fem),
               padding:
-                  EdgeInsets.fromLTRB(18 * fem, 24 * fem, 20 * fem, 7 * fem),
+                  EdgeInsets.fromLTRB(18 * fem, 24 * fem, 20 * fem, 0 * fem),
               width: double.infinity,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(15 * fem),
@@ -194,11 +197,90 @@ class Homepage extends StatelessWidget {
                 ],
               ),
             ),
-            const SizedBox(height: 25),
+            const SizedBox(height: 30),
+
+            //weather and contact widget
+            Row(children: [
+              Container(
+                margin:
+                    EdgeInsets.fromLTRB(5 * fem, 0 * fem, 20 * fem, 0 * fem),
+                padding:
+                    EdgeInsets.fromLTRB(2 * fem, 35 * fem, 20 * fem, 0 * fem),
+                width: 170 * fem,
+                height: 170,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15 * fem),
+                  image: const DecorationImage(
+                    image: AssetImage('assets/images/contact.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const AiBotScreen()),
+                        );
+                      },
+                      child: Text(
+                        'Book your Appointment',
+                        style: TextStyle(
+                          fontSize: 17 * ffem,
+                          fontWeight: FontWeight.w600,
+                          height: 1 * ffem / fem,
+                          color: Color.fromARGB(255, 0, 0, 0),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(0 * fem, 0 * fem, 6 * fem, 0 * fem),
+                padding:
+                    EdgeInsets.fromLTRB(2 * fem, 35 * fem, 20 * fem, 0 * fem),
+                width: 170 * fem,
+                height: 170,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15 * fem),
+                  image: const DecorationImage(
+                    image: AssetImage('assets/images/weather.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const WeatherWidget()),
+                        );
+                      },
+                      child: Text(
+                        'Weather',
+                        style: TextStyle(
+                          fontSize: 17 * ffem,
+                          fontWeight: FontWeight.w600,
+                          height: 1 * ffem / fem,
+                          color: Color.fromARGB(255, 0, 0, 0),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ]),
 
             // Newsletter Section
             Container(
-              margin: EdgeInsets.fromLTRB(0 * fem, 0 * fem, 0 * fem, 0 * fem),
+              margin: EdgeInsets.fromLTRB(0 * fem, 25 * fem, 0 * fem, 0 * fem),
               padding: EdgeInsets.all(5 * fem),
               width: double.infinity,
               decoration: BoxDecoration(
@@ -217,7 +299,7 @@ class Homepage extends StatelessWidget {
                       color: const Color(0xff02841e),
                     ),
                   ),
-                  SizedBox(height: 10 * fem),
+                  SizedBox(height: 1 * fem),
                   const NewsletterWidget(), // Use your NewsletterWidget here
                 ],
               ),
