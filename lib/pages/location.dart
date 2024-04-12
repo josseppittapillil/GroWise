39d8d58118a320da79b0a95be18da433d555528c
Nitin-Tomas31 <133589606+Nitin-Tomas31@users.noreply.github.com>
2024-04-12 11:24:22 +0530

    Update location.dart

--- lib/pages/location.dart
+++ lib/pages/location.dart
@@ -1,9 +1,15 @@
+import 'dart:convert';
+import 'prompts.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:location/location.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'home.dart';
+import 'package:http/http.dart' as http;
+
+String loc = "";
+String pla = "123";
 
 class Maps extends StatefulWidget {
   const Maps({Key? key}) : super(key: key);
@@ -34,7 +40,8 @@ class MyHomePage extends StatelessWidget {
   final MapController mapController;
   final List<Marker> markers;
 
-  MyHomePage({Key? key, required this.mapController, required this.markers})
+  const MyHomePage(
+      {Key? key, required this.mapController, required this.markers})
       : super(key: key);
 
   @override
@@ -112,6 +119,7 @@ class MyHomePage extends StatelessWidget {
       gravity: ToastGravity.BOTTOM,
       timeInSecForIosWeb: 3,
     );
+    loc = "${currentLocation.latitude},${currentLocation.longitude}";
 
     // Add a marker at the current location
     markers.add(
@@ -151,6 +159,350 @@ class MyHomePage extends StatelessWidget {
           ],
         );
       },
+    );
+  }
+}
+
+class WeatherInfoWidget extends StatefulWidget {
+  const WeatherInfoWidget({Key? key}) : super(key: key);
+
+  @override
+  _WeatherInfoWidgetState createState() => _WeatherInfoWidgetState();
+}
+
+class _WeatherInfoWidgetState extends State<WeatherInfoWidget> {
+  late String location = '';
+  late String weatherCondition = '';
+  late double temperature = 0.0;
+  late String localtime = '';
+  late double windSpeed = 0.0;
+  late String iconUrl = '';
+  bool isLoading = true; // Indicates whether data is being loaded
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    const apiKey = 'f2957dcefa844bf7a83152148240204';
+    var apiUrl =
+        'http://api.weatherapi.com/v1/current.json?key=$apiKey&q=$loc&aqi=no';
+
+    try {
+      final response = await http.get(Uri.parse(apiUrl));
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = json.decode(response.body);
+
+        setState(() {
+          location = data['location']['name'];
+          print("Location retrieved: $location");
+          weatherCondition = data['current']['condition']['text'];
+          temperature = data['current']['temp_c'];
+          localtime = data['location']['localtime'];
+          windSpeed = data['current']['wind_kph'];
+          iconUrl = 'https:' + data['current']['condition']['icon'];
+          isLoading = false;
+          pla = location;
+          print("confirmation: $pla"); // Data loaded successfully
+        });
+      } else {
+        // Handle error, show error message or retry logic
+        print('Error: ${response.statusCode}');
+        setState(() {
+          isLoading = false; // Error occurred while fetching data
+        });
+      }
+    } catch (e) {
+      // Handle network or other errors
+      print('Error: $e');
+      setState(() {
+        isLoading = false; // Error occurred while fetching data
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 170,
+      height: 170,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20.0),
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            Image.asset(
+              'assets/images/weather.jpg',
+              width: 170,
+              height: 170,
+              fit: BoxFit.cover,
+            ),
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: isLoading
+                  ? const Center(child: CircularProgressIndicator())
+                  : Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          location,
+                          style: const TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 0, 0, 0),
+                          ),
+                        ),
+                        const SizedBox(height: 4.0),
+                        Text(
+                          weatherCondition,
+                          style: const TextStyle(
+                            fontSize: 14.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                        const SizedBox(height: 4.0),
+                        Text(
+                          '$temperature°C',
+                          style: const TextStyle(
+                            fontSize: 14.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                        const SizedBox(height: 4.0),
+                        Text(
+                          localtime,
+                          style: const TextStyle(
+                            fontSize: 14.0,
+                            color: Color.fromARGB(255, 0, 0, 0),
+                          ),
+                        ),
+                        const SizedBox(height: 4.0),
+                        Image.network(
+                          iconUrl,
+                          width: 39,
+                        )
+                      ],
+                    ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AiBotScreen extends StatefulWidget {
+  const AiBotScreen({Key? key}) : super(key: key);
+
+  @override
+  _AiBotScreenState createState() => _AiBotScreenState();
+}
+
+class _AiBotScreenState extends State<AiBotScreen> {
+  final TextEditingController _messageController = TextEditingController();
+  final List<ChatMessage> _messages = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: const Text(
+          'GroWise Bot',
+          style: TextStyle(color: Colors.black),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.chat, color: Colors.black),
+            onPressed: () {
+              // Handle chat icon press
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.black,
+              child: ListView.builder(
+                itemCount: _messages.length,
+                reverse: true,
+                itemBuilder: (context, index) {
+                  return _messages[index];
+                },
+              ),
+            ),
+          ),
+          _buildMessageBar(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMessageBar() {
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      color: Colors.white,
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _messageController,
+              decoration: const InputDecoration(
+                hintText: 'Type a message...',
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+          IconButton(
+            icon: const Icon(Icons.send),
+            onPressed: () {
+              _sendMessage();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _sendMessage() async {
+    String message = _messageController.text.trim();
+    String location = pla;
+    print("ai loc: $location"); // Replace with your location retrieval logic
+
+    if (message.isNotEmpty) {
+      setState(() {
+        _messages.insert(0, ChatMessage(text: message, isUser: true));
+        _messageController.clear();
+      });
+
+      if (_isGreeting(message)) {
+        String response =
+            "Hello, I am your personal agricultural assistant. How may I assist you today?";
+        _addResponse(response);
+      } else if (_isAgricultureQuery(message)) {
+        // Check if the query contains "recommend" and "crops"
+        if (message.toLowerCase().contains("recommend") &&
+            message.toLowerCase().contains("crops")) {
+          print("Location retrieved: $location");
+          if (location.isNotEmpty) {
+            message += " in $location"; // Append location to the query
+            print("Final query with location: $message");
+          } // Append location to the query
+        }
+        String aiResponse = await getAIResponse(message);
+        _addResponse(aiResponse);
+      } else {
+        String response =
+            "This Assistant is Fine-Tuned for your Agricultural Queries and does not have Knowledge beyond the topic.";
+        _addResponse(response);
+      }
+    }
+  }
+
+  bool _isGreeting(String message) {
+    for (String greeting in Constants.greetings) {
+      if (message.trim().toLowerCase() == greeting) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  bool _isAgricultureQuery(String message) {
+    for (String keyword in Constants.agricultureKeywords) {
+      if (message.toLowerCase().contains(keyword)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void _addResponse(String text) {
+    setState(() {
+      _messages.insert(0, ChatMessage(text: text, isUser: false));
+    });
+  }
+
+  Future<String> getAIResponse(String message) async {
+    String url = 'https://api.openai.com/v1/completions';
+    Map<String, String> headers = {
+      'Content-Type': 'application/json',
+      'Authorization':
+          'Bearer xxxxx',
+    };
+    Map<String, dynamic> body = {
+      'model': 'gpt-3.5-turbo-instruct', // DialoGPT model
+      'prompt': message,
+      'max_tokens': 150, // Adjust based on the desired response length
+    };
+
+    http.Response response = await http.post(
+      Uri.parse(url),
+      headers: headers,
+      body: json.encode(body),
+    );
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = json.decode(response.body);
+      return data['choices'][0]['text'].toString();
+    } else {
+      print('Failed to get AI response: ${response.statusCode}');
+      print('Response body: ${response.body}');
+      throw Exception('Failed to get AI response');
+    }
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  final String text;
+  final bool isUser;
+
+  const ChatMessage({required this.text, required this.isUser, Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 5),
+      padding: const EdgeInsets.all(8.0),
+      alignment: isUser ? Alignment.centerRight : Alignment.centerLeft,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: 10,
+            height: 10,
+            margin: const EdgeInsets.only(top: 8, right: 8),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color:
+                  isUser ? Colors.blue : const Color.fromARGB(255, 61, 179, 35),
+            ),
+          ),
+          Flexible(
+            child: Text(
+              text,
+              style: TextStyle(
+                color: isUser
+                    ? const Color.fromARGB(255, 251, 251, 251)
+                    : const Color.fromARGB(255, 255, 255, 255),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
