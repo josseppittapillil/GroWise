b5fcd1d35a6502ec48433fbc4b819a85816f0ec0
Josse P Pittappillil <65817274+josseppittapillil@users.noreply.github.com>
2024-04-15 15:21:36 +0530

    appointment booking details added to firebase

--- lib/pages/book.dart
+++ lib/pages/book.dart
@@ -1,132 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:credentials_manager/credentials_manager.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class AppointmentBookingPage extends StatefulWidget {
-  @override
-  _AppointmentBookingPageState createState() => _AppointmentBookingPageState();
-}
+class AppointmentBookingPage extends StatelessWidget {
+  final _nameController = TextEditingController();
+  final _addressController = TextEditingController();
+  final _contactDetailsController = TextEditingController();
+  final _dateController = TextEditingController();
+  final _timeController = TextEditingController();
 
-class _AppointmentBookingPageState extends State<AppointmentBookingPage> {
-  final _formKey = GlobalKey<FormState>();
-  String _appointmentType = 'Soil test';
-  String _name = '';
-  String _address = '';
-  DateTime? _selectedDate;
-  TimeOfDay? _selectedTime;
-  String _phone = '';
+  Future<void> _submitForm(BuildContext context) async {
+    final name = _nameController.text;
+    final address = _addressController.text;
+    final contactDetails = _contactDetailsController.text;
+    final date = _dateController.text;
+    final time = _timeController.text;
 
-  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-      FlutterLocalNotificationsPlugin();
+    if (name.isNotEmpty &&
+        address.isNotEmpty &&
+        contactDetails.isNotEmpty &&
+        date.isNotEmpty &&
+        time.isNotEmpty) {
+      await FirebaseFirestore.instance.collection('client').add({
+        'name': name,
+        'address': address,
+        'contact_details': contactDetails,
+        'date': date,
+        'time': time,
+      });
 
-  @override
-  void initState() {
-    super.initState();
-    _initializeNotifications();
-  }
-
-  Future<void> _initializeNotifications() async {
-    const AndroidInitializationSettings initializationSettingsAndroid =
-        AndroidInitializationSettings('@mipmap/ic_launcher');
-    const InitializationSettings initializationSettings =
-        InitializationSettings(android: initializationSettingsAndroid);
-    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
-  }
-
-  void _submitForm() async {
-    if (_formKey.currentState!.validate()) {
-      final credentialsManager = CredentialsManager(
-        storageKey: 'storage_key',
-        useAndroidEncryptedSharedPreferences: true,
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text('Booking Successful'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                Navigator.pop(context);
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        ),
       );
-
-      final savedCredentials = await credentialsManager.getSavedCredentials();
-
-      String? username = savedCredentials.isNotEmpty
-          ? savedCredentials.first.loginOrEmail
-          : null;
-
-      if (username != null) {
-        String bookingKey = '${username}_appointment';
-
-        SharedPreferences prefs = await SharedPreferences.getInstance();
-
-        DateTime appointmentDateTime = DateTime(
-          _selectedDate!.year,
-          _selectedDate!.month,
-          _selectedDate!.day,
-          _selectedTime!.hour,
-          _selectedTime!.minute,
-        );
-
-        await prefs.setString(
-          bookingKey,
-          '$_appointmentType - $_name - $_address - $appointmentDateTime - $_phone',
-        );
-
-        showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: const Text('Booking Successful'),
-              actions: <Widget>[
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                    Navigator.pop(context);
-                  },
-                  child: const Text('OK'),
-                ),
-              ],
-            );
-          },
-        );
-      } else {
-        showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: const Text('Error'),
-              content: const Text('No user credentials found.'),
-              actions: <Widget>[
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: const Text('OK'),
-                ),
-              ],
-            );
-          },
-        );
-      }
-    }
-  }
-
-  Future<void> _selectDate(BuildContext context) async {
-    final DateTime? picked = await showDatePicker(
-      context: context,
-      initialDate: _selectedDate ?? DateTime.now(),
-      firstDate: DateTime.now(),
-      lastDate: DateTime(DateTime.now().year + 1),
-    );
-    if (picked != null && picked != _selectedDate) {
-      setState(() {
-        _selectedDate = picked;
-      });
-    }
-  }
-
-  Future<void> _selectTime(BuildContext context) async {
-    final TimeOfDay? picked = await showTimePicker(
-      context: context,
-      initialTime: _selectedTime ?? TimeOfDay.now(),
-    );
-    if (picked != null && picked != _selectedTime) {
-      setState(() {
-        _selectedTime = picked;
-      });
     }
   }
 
@@ -142,124 +58,74 @@ class _AppointmentBookingPageState extends State<AppointmentBookingPage> {
       body: Container(
         padding: const EdgeInsets.all(20.0),
         color: Colors.white,
-        child: Form(
-          key: _formKey,
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                DropdownButtonFormField<String>(
-                  value: _appointmentType,
-                  onChanged: (newValue) {
-                    setState(() {
-                      _appointmentType = newValue!;
-                    });
-                  },
-                  items: <String>['Soil test', 'Field Check']
-                      .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  decoration: const InputDecoration(
-                    labelText: 'Select type of appointment:',
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              TextFormField(
+                controller: _nameController,
+                decoration: const InputDecoration(
+                  labelText: 'Name',
+                ),
+              ),
+              TextFormField(
+                controller: _addressController,
+                decoration: const InputDecoration(labelText: 'Address'),
+              ),
+              GestureDetector(
+                onTap: () async {
+                  final picked = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime.now(),
+                    lastDate: DateTime.now().add(Duration(days: 365)),
+                  );
+                  if (picked != null) {
+                    _dateController.text =
+                        '${picked.day}/${picked.month}/${picked.year}';
+                  }
+                },
+                child: AbsorbPointer(
+                  child: TextFormField(
+                    decoration: const InputDecoration(labelText: 'Date'),
+                    controller: _dateController,
                   ),
                 ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Name',
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter your name';
-                    }
-                    return null;
-                  },
-                  onChanged: (value) {
-                    _name = value;
-                  },
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Address'),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter your address';
-                    }
-                    return null;
-                  },
-                  onChanged: (value) {
-                    _address = value;
-                  },
-                ),
-                GestureDetector(
-                  onTap: () => _selectDate(context),
-                  child: AbsorbPointer(
-                    child: TextFormField(
-                      decoration: const InputDecoration(labelText: 'Date'),
-                      validator: (value) {
-                        if (_selectedDate == null) {
-                          return 'Please select a date';
-                        }
-                        return null;
-                      },
-                      controller: TextEditingController(
-                        text: _selectedDate != null
-                            ? '${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year}'
-                            : '',
-                      ),
-                    ),
+              ),
+              GestureDetector(
+                onTap: () async {
+                  final picked = await showTimePicker(
+                    context: context,
+                    initialTime: TimeOfDay.now(),
+                  );
+                  if (picked != null) {
+                    _timeController.text = '${picked.hour}:${picked.minute}';
+                  }
+                },
+                child: AbsorbPointer(
+                  child: TextFormField(
+                    decoration: const InputDecoration(labelText: 'Time'),
+                    controller: _timeController,
                   ),
                 ),
-                GestureDetector(
-                  onTap: () => _selectTime(context),
-                  child: AbsorbPointer(
-                    child: TextFormField(
-                      decoration: const InputDecoration(labelText: 'Time'),
-                      validator: (value) {
-                        if (_selectedTime == null) {
-                          return 'Please select a time';
-                        }
-                        return null;
-                      },
-                      controller: TextEditingController(
-                        text: _selectedTime != null
-                            ? '${_selectedTime!.hour}:${_selectedTime!.minute}'
-                            : '',
-                      ),
-                    ),
-                  ),
+              ),
+              TextFormField(
+                controller: _contactDetailsController,
+                decoration: const InputDecoration(labelText: 'Contact Details'),
+                keyboardType: TextInputType.phone,
+                maxLength: 10,
+              ),
+              const SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () => _submitForm(context),
+                style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Colors.green)),
+                child: const Text(
+                  'Book Now',
+                  style: TextStyle(color: Colors.black),
                 ),
-                TextFormField(
-                  decoration:
-                      const InputDecoration(labelText: 'Contact Details'),
-                  keyboardType: TextInputType.phone,
-                  maxLength: 10,
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter your phone number';
-                    }
-                    if (value.length < 10) {
-                      return 'Phone number should be 10 digits';
-                    }
-                    return null;
-                  },
-                  onChanged: (value) {
-                    _phone = value;
-                  },
-                ),
-                const SizedBox(height: 20.0),
-                ElevatedButton(
-                  onPressed: _submitForm,
-                  style: const ButtonStyle(
-                      backgroundColor: MaterialStatePropertyAll(Colors.green)),
-                  child: const Text(
-                    'Book Now',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
