3771aaf828ec0a955b04d67396faad0ca3562074
Nitin-Tomas31 <133589606+Nitin-Tomas31@users.noreply.github.com>
2024-04-17 02:39:46 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,12 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:growise/pages/login.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:growwise/pages/front.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -49,7 +45,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.white,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -85,4 +81,3 @@ class _SplashScreenState extends State<SplashScreen> {
     );
   }
 }
-
