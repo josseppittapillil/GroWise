591f7ee4dd64b1f59932ca26176e43899aadca3c
Nitin-Tomas31 <133589606+Nitin-Tomas31@users.noreply.github.com>
2024-04-17 04:10:33 +0530

    Update home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -181,11 +181,52 @@ class Info2Page extends StatelessWidget {
               'The integrated marketplace provides a convenient platform for purchasing essential supplies, equipment, and services directly from trusted vendors to thrive in the agricultural sector.',
               style: TextStyle(
                 color: Colors.black,
-                fontSize: 16,
+                fontSize: 18,
               ),
               textAlign: TextAlign.center,
             ),
             SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Info3Page extends StatelessWidget {
+  const Info3Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0, // Remove shadow
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 20),
+            const Text(
+              '"Appointment Booking feature, designed to streamline your agricultural operations"',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 50),
+            Image.asset(
+              'assets/images/info3.jpg',
+              height: 350,
+              width: 350,
+              fit: BoxFit.contain,
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              "“Easily schedule crop consultations, veterinary visits, equipment maintenance, and more—all from your phone. Simplify your appointments and manage them effortlessly with our app.”",
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
           ],
         ),
       ),
@@ -206,6 +247,7 @@ class _HomeScreenState extends State<HomeScreen> {
   final List<Widget> _screens = [
     const Info1Page(),
     const Info2Page(),
+    const Info3Page()
   ];
 
   void _nextPage() {
@@ -227,9 +269,16 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _screens[_currentIndex],
-      floatingActionButton: FloatingActionButton(
+      floatingActionButton: TextButton(
         onPressed: _nextPage,
-        child: const Icon(Icons.navigate_next),
+        style: const ButtonStyle(alignment: Alignment.bottomCenter),
+        child: const Text(
+          "NEXT >",
+          style: TextStyle(
+              color: Color.fromRGBO(2, 132, 30, 1),
+              fontSize: 20,
+              fontWeight: FontWeight.bold),
+        ),
       ),
     );
   }
