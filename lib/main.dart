d162411b73da7984af6e5e05c8eb089db515ed12
Nitin-Tomas31 <133589606+Nitin-Tomas31@users.noreply.github.com>
2024-04-16 21:42:33 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -41,28 +41,48 @@ class _SplashScreenState extends State<SplashScreen> {
     Future.delayed(const Duration(seconds: 5), () {
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => const LoginPage()),
+        MaterialPageRoute(builder: (context) => const FrontPage()),
       );
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.white,
+    return Scaffold(
+      backgroundColor: Colors.black,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            FlutterLogo(size: 100), // Replace with your app logo or image
-            SizedBox(height: 16),
-            Text(
-              'GroWise',
-              style: TextStyle(fontSize: 24),
+            const Column(
+              children: [
+                SizedBox(
+                  height: 300,
+                ),
+                Image(
+                  image: AssetImage('assets/images/logo.png'),
+                ),
+                Text(
+                  'GroWise',
+                  style: TextStyle(fontSize: 24, color: Colors.green),
+                ),
+              ],
             ),
+            SizedBox(
+                height: MediaQuery.of(context).size.height *
+                    0.25), // Adjust the height based on your requirement
+            const Text(
+              'Since',
+              style: TextStyle(fontSize: 19, color: Colors.white),
+            ),
+            const Text(
+              '2024',
+              style: TextStyle(fontSize: 22, color: Colors.green),
+            )
           ],
         ),
       ),
     );
   }
 }
+
